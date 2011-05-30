448cb0616e967324c61d66fb3f54015a14b08cae
Pierre Merienne <pierre.merienne@excilys.com>
2011-05-30 19:26:51 +0200

    Ajout d'instruction de navigation sur le slide d'introduction de la demo

--- src/com/excilys/formation/gwt/client/formation/demo/Introduction.java
+++ src/com/excilys/formation/gwt/client/formation/demo/Introduction.java
@@ -6,11 +6,12 @@ import com.excilys.formation.gwt.client.formation.introduction.Introduction.Conc
 import com.excilys.formation.gwt.client.formation.introduction.Introduction.Fonctionnement;
 import com.excilys.formation.gwt.client.formation.introduction.Introduction.Historique;
 import com.excilys.formation.gwt.client.formation.introduction.Introduction.Limitations;
-import com.excilys.formation.gwt.client.formation.introduction.Introduction.Titre;
 import com.excilys.formation.gwt.client.formation.introduction.TropSimple;
+import com.excilys.formation.gwt.client.slider.shownotes.ElementSlideNotes;
 import com.excilys.formation.gwt.client.slider.slides.Chapter;
 import com.excilys.formation.gwt.client.slider.slides.WidgetSlide;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.Widget;
@@ -20,6 +21,11 @@ public class Introduction extends Chapter {
     @UiTemplate("Plan.ui.xml")
     public interface Plan extends UiBinder<Widget, WidgetSlide> {
         Plan binder = GWT.create(Plan.class);
+    }
+
+    @UiTemplate("Titre.ui.xml")
+    public interface Titre extends UiBinder<Element, ElementSlideNotes> {
+        Titre binder = GWT.create(Titre.class);
     }
 
     @Override
