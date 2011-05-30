ba3f9d78999a9a4690f57ea9c5b6daba0dc049ec
Merienne <pmerienne@excilys-laptop.(none)>
2011-05-30 17:35:14 +0200

    Ajout de la charte graphique excilys

--- src/com/excilys/formation/gwt/client/slider/Resources.java
+++ src/com/excilys/formation/gwt/client/slider/Resources.java
@@ -4,6 +4,7 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.NotStrict;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface Resources extends ClientBundle {
 
@@ -15,4 +16,18 @@ public interface Resources extends ClientBundle {
     @NotStrict
     CssResource shownotes();
 
+    @Source("com/excilys/formation/gwt/assets/slide-bg.png")
+    ImageResource slideBg();
+
+    @Source("com/excilys/formation/gwt/assets/title-puce-top.png")
+    ImageResource learningIcon();
+
+    @Source("com/excilys/formation/gwt/assets/logo-ebi-top.png")
+    ImageResource ebiIcon();
+
+    @Source("com/excilys/formation/gwt/assets/logo-exc-grp-bottom.png")
+    ImageResource excilysIcon();
+
+    @Source("com/excilys/formation/gwt/assets/plan-bg.png")
+    ImageResource planBg();
 }
